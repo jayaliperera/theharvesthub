c3cc405520ed4181b9457227c685b715060fba79
Rithmi <s92069470@ousl.lk>
2024-09-09 22:39:19 +0530

    feat:create login screen

---
+++ lib/screens/loginpage.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:theharvesthub/forgot_password_screen.dart';
+import 'package:theharvesthub/home_screen.dart';
+import 'packagetheharvesthub/signup_screen.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  // Method to handle forgot password action
+  void _handleForgotPassword(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const ForgotPasswordScreen()),
+    );
+  }
+
+  InputDecoration customInputDecoration(String labelText, {bool isPasswordField = false}) {
+    return InputDecoration(
+      labelText: labelText,
+      filled: true,
+      fillColor: Colors.white,
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(16.0),
+        borderSide: BorderSide.none,
+      ),
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(16.0),
+        borderSide: BorderSide.none,
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(16.0),
+        borderSide: BorderSide.none,
+      ),
+      contentPadding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 15.0),
+      floatingLabelBehavior: FloatingLabelBehavior.auto,
+      labelStyle: TextStyle(color: Colors.grey[700]),
+      hintStyle: TextStyle(color: Colors.grey[400]),
+    );
+  }
+
+}
