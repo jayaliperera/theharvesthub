5d0ba390979b22a0d353dd74bf871df2e7a5b8bc
dilininimesha <dilinimalawaraarachchi@gmail.com>
2024-12-14 21:21:22 +0530

    edit logging screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:theharvesthub/screens/buyer%20screens/buyer_dashboard.dart';
 import 'package:theharvesthub/screens/forgot_password_screen.dart';
 import 'package:theharvesthub/screens/seller%20screens/product_listing.dart';
 import 'package:theharvesthub/screens/signup_screen.dart';
