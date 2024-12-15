f7327dc76326d95f98ff3016f3541248fa9bd8b3
Jayali <jayalilaknaperera@gmail.com>
2024-12-15 18:23:43 +0530

    feat: implement product screen, change the navigation

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:theharvesthub/screens/buyer%20screens/buyer_dashboard.dart';
 import 'package:theharvesthub/screens/forgot_password_screen.dart';
 import 'package:theharvesthub/screens/seller%20screens/product_listing.dart';
 import 'package:theharvesthub/screens/signup_screen.dart';
@@ -126,7 +127,7 @@ class LoginScreen extends StatelessWidget {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                          builder: (context) => const product_listing()),
+                          builder: (context) => const BuyerDashboard()),
                     );
                   },
                   style: ElevatedButton.styleFrom(
