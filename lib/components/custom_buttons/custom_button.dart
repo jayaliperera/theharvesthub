a051af94a0c392922c726c3aede8f6f7f18120d4
jayali.p <jayali.perera@omobio.net>
2025-02-23 15:35:04 +0530

    feat: custom button changes

--- lib/components/custom_buttons/custom_button.dart
+++ lib/components/custom_buttons/custom_button.dart
@@ -1,38 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:theharvesthub/components/custom_texts/custom_text.dart';
 
-import '../cusrom_text/custom_popins_text.dart';
-
-class CustomButton1 extends StatelessWidget {
-  const CustomButton1({
-    super.key,
-    required this.size,
-    required this.text,
-    required this.bgColor,
-    required this.onTap,
-  });
+class CustomButton extends StatelessWidget {
+  const CustomButton(
+      {super.key,
+      required this.size,
+      required this.text,
+      required this.bgColor});
 
   final Size size;
   final String text;
   final Color bgColor;
-  final VoidCallback onTap;
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: GestureDetector(
-        onTap: onTap,
-        child: Container(
-          width: size.width * 0.7,
-          height: 55,
-          decoration: BoxDecoration(
-              color: Colors.orange.shade800,
-              borderRadius: BorderRadius.circular(15)),
-          child: Center(
-            child: CustomPopinsText(
-              text: text,
-              fontSize: 18,
-              color: Colors.white,
-            ),
+      child: Container(
+        width: size.width * 0.7,
+        height: 45,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10), color: bgColor),
+        child:  Center(
+          child: CustomText(
+            text:text,
+            fontSize: 20,
+            fontWeight: FontWeight.w400,
+            color: Colors.white,
           ),
         ),
       ),
