40eedd4dfc95c7985ef355dd58171e6d828f170c
jayali.p <jayali.perera@omobio.net>
2025-02-23 20:06:13 +0530

    adding custom navigator

---
+++ lib/utills/custom_navigators.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class CustomNavigators {
+
+  static void goTo(BuildContext context, Widget widget){
+    Navigator.push(context, MaterialPageRoute(builder: (context) => widget));
+  }
+}
