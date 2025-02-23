f5d86a14877115a18f0a7e13ad9fecbec72d880c
jayali.p <jayali.perera@omobio.net>
2025-02-23 21:23:48 +0530

    feat: product model

---
+++ lib/models/product_model.dart
@@ -0,0 +1,15 @@
+class ProductModel {
+  int id;
+  String title;
+  String description;
+  String image;
+  double price;
+
+  ProductModel({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.image,
+    required this.price,
+  });
+}
