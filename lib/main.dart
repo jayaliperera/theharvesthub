50291e757d9ff1108eaa0574e3d0f54c13fa356b
jayali.p <jayali.perera@omobio.net>
2025-02-24 00:17:00 +0530

    refactor: import splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:theharvesthub/firebase_options.dart';
-import 'package:theharvesthub/screens/common%20screens/splash_screen.dart';
+import 'package:theharvesthub/screens/splash_screen/splash_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
