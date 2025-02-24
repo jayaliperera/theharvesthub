52bc3187fd3e595b2e427a72be8dbe91f6c0e2c7
Rithmi <s92069470@ousl.lk>
2025-02-24 10:00:34 +0530

    Enhanced sign in page

--- lib/screens/auth_screen/signin_page.dart
+++ lib/screens/auth_screen/signin_page.dart
@@ -1,3 +1,5 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
 import 'package:theharvesthub/components/custom_buttons/custom_button.dart';
 import 'package:theharvesthub/components/custom_text_fields/custom_text_fields.dart';
@@ -22,102 +24,131 @@ class _SignInPageState extends State<SignInPage> {
   Widget build(BuildContext context) {
     final size = MediaQuery.sizeOf(context);
     return Scaffold(
-        body: SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const CustomText(
-              text: "The Harvest Hub",
-              fontSize: 30,
-              fontWeight: FontWeight.w500,
-            ),
-            const CustomText(
-              text: "Please fill your details to access the account.",
-              fontSize: 15,
-              fontWeight: FontWeight.w400,
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            CustomTextField(
-              labelText: "Email",
-              hintText: "Enter your Email",
-              prefixIcon: const Icon(Icons.email),
-              controller: emailController,
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            CustomTextField(
-              labelText: "Password",
-              hintText: "Enter your password",
-              prefixIcon: const Icon(Icons.password),
-              isPassword: true,
-              controller: passwordController,
-            ),
-            const SizedBox(
-              height: 5,
-            ),
-            Row(
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 24.0, vertical: 20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Checkbox(
-                  value: false,
-                  onChanged: (val) {},
-                  shape: const OvalBorder(),
+                // Logo
+                Image.asset(
+                  'assets/images/logo.png',
+                  height: 100,
                 ),
+                const SizedBox(height: 20),
+
+                // Title
                 const CustomText(
-                    text: "Remeber me",
-                    fontSize: 15,
-                    fontWeight: FontWeight.w400),
-                const Spacer(),
-                GestureDetector(
-                  onTap: () {
-                    CustomNavigators.goTo(context, const ForgotPasswordPage());
-                  },
-                  child: const CustomText(
-                    text: "Forgot Password?",
-                    fontSize: 15,
-                    fontWeight: FontWeight.w400,
-                    color: Colors.greenAccent,
-                  ),
+                  text: "The Harvest Hub",
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
                 ),
                 const SizedBox(height: 10),
+                const CustomText(
+                  text: "Please fill in your details to access your account.",
+                  fontSize: 16,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.grey,
+                ),
+                const SizedBox(height: 30),
+
+                // Email Field
+                CustomTextField(
+                  labelText: "Email",
+                  hintText: "Enter your Email",
+                  prefixIcon: const Icon(Icons.email),
+                  controller: emailController,
+                ),
+                const SizedBox(height: 15),
+
+                // Password Field
+                CustomTextField(
+                  labelText: "Password",
+                  hintText: "Enter your password",
+                  prefixIcon: const Icon(Icons.lock),
+                  isPassword: true,
+                  controller: passwordController,
+                ),
+                const SizedBox(height: 10),
+
+                // Remember Me & Forgot Password
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Checkbox(
+                          value: false,
+                          onChanged: (val) {},
+                          shape: const CircleBorder(),
+                        ),
+                        const CustomText(
+                          text: "Remember me",
+                          fontSize: 15,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ],
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        CustomNavigators.goTo(
+                            context, const ForgotPasswordPage());
+                      },
+                      child: const CustomText(
+                        text: "Forgot Password?",
+                        fontSize: 15,
+                        fontWeight: FontWeight.w400,
+                        color: Colors.green,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20),
+
+                // Sign In Button
+                GestureDetector(
+                  onTap: () {
+                    CustomNavigators.goTo(context, const HomePage());
+                  },
+                  child: CustomButton(
+                    text: "Sign in",
+                    bgColor: Colors.green.shade700,
+                    size: size,
+                  ),
+                ),
+                const SizedBox(height: 20),
+
+                // Sign Up Link
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const CustomText(
+                      text: "Don't have an account? ",
+                      fontSize: 15,
+                      fontWeight: FontWeight.w400,
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        CustomNavigators.goTo(context, const SignUpPage());
+                      },
+                      child: const CustomText(
+                        text: "Sign Up",
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.green,
+                      ),
+                    ),
+                  ],
+                ),
               ],
             ),
-            const SizedBox(
-              height: 10,
-            ),
-            GestureDetector(
-              onTap: () {
-                CustomNavigators.goTo(context, const HomePage());
-              },
-              child: CustomButton(
-                  text: "Sign in", bgColor: Colors.green.shade700, size: size),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Center(
-                child: InkWell(
-              onTap: () {
-                CustomNavigators.goTo(context, const SignUpPage());
-              },
-              child: const Text.rich(TextSpan(
-                  text: "Don't you have an account? ",
-                  children: [
-                    TextSpan(
-                        text: "Sign Up", style: TextStyle(color: Colors.green))
-                  ])),
-            ))
-          ],
+          ),
         ),
       ),
-    ));
+    );
   }
 }
