4bf0b23d0a8f8b1f66747a6ccc267e6841455f57
jayali.p <jayali.perera@omobio.net>
2025-02-28 23:46:42 +0530

    feat:create db instance and collection to create users

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -9,6 +10,8 @@ import 'package:theharvesthub/providers/auth_provider.dart' as local;
 
 
 class AuthController {
+CollectionReference users = FirebaseFirestore.instance.collection('Users');
+
   Future<void> listenAuthState(BuildContext context) async {
     FirebaseAuth.instance.authStateChanges().listen((User? user) {
       if (user == null) {
