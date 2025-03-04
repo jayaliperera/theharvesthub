90ba7a67315c71275f1916428630eab4187d9f45
dilininimesha <dilinimalawaraarachchi@gmail.com>
2025-03-04 21:12:39 +0530

    cart screen padding

--- lib/screens/home_screen/cart/cart_screen.dart
+++ lib/screens/home_screen/cart/cart_screen.dart
@@ -19,7 +19,7 @@ class _CartScreenState extends State<CartScreen> {
     return Scaffold(
       body: SafeArea(
         child: Padding(
-          padding: EdgeInsets.all(8.0),
+          padding: const EdgeInsets.all(8.0),
           child: Column(
             children: [
               const Row(
