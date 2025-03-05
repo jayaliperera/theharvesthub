cd04246721420a84fe71b5d0a4e0c2a5b0a1ac0c
dilininimesha <dilinimalawaraarachchi@gmail.com>
2025-03-05 22:16:34 +0530

    edit ui

--- lib/components/custom_buttons/custom_button.dart
+++ lib/components/custom_buttons/custom_button.dart
@@ -7,26 +7,32 @@ class CustomButton extends StatelessWidget {
       required this.size,
       required this.text,
       required this.bgColor,
-      required Null Function() onTap});
+      required this.onTap});
 
   final Size size;
   final String text;
   final Color bgColor;
+  final VoidCallback onTap;
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Container(
-        width: size.width * 0.7,
-        height: 45,
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10), color: bgColor),
-        child: Center(
-          child: CustomText(
-            text: text,
-            fontSize: 20,
-            fontWeight: FontWeight.w400,
-            color: Colors.white,
+      child: GestureDetector(
+        onTap: onTap,
+        child: Container(
+          width: size.width * 0.7,
+          height: 45,
+          decoration: BoxDecoration(
+            color: bgColor,
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: Center(
+            child: CustomText(
+              text: text,
+              fontSize: 15,
+              fontWeight: FontWeight.w400,
+              color: Colors.white,
+            ),
           ),
         ),
       ),
