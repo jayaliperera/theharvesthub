068db07a8de8f4f2c9a23b6e249bbb0cffeb90ef
jayali.p <jayali.perera@omobio.net>
2025-03-10 22:09:11 +0530

    refactor: refactor unwanted code snippets

--- lib/utills/custom_navigators.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class CustomNavigators {
-
-  static void goTo(BuildContext context, Widget widget){
-    Navigator.push(context, MaterialPageRoute(builder: (context) => widget));
-  }
-}
