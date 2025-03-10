5a81e6f6c5cd72d20c950eb11b946e74029ecb61
jayali.p <jayali.perera@omobio.net>
2025-03-10 22:27:23 +0530

    refactor: add database helper

---
+++ lib/Database/db_helper.dart
@@ -0,0 +1,65 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class DatabaseHelper {
+  static final DatabaseHelper _instance = DatabaseHelper._internal();
+  static Database? _database;
+
+  factory DatabaseHelper() {
+    return _instance;
+  }
+
+  DatabaseHelper._internal();
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDatabase();
+    return _database!;
+  }
+
+  Future<Database> _initDatabase() async {
+    String path = join(await getDatabasesPath(), 'signup.db');
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: _onCreate,
+    );
+  }
+
+  Future<void> _onCreate(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        displayName TEXT,
+        address TEXT,
+        username TEXT,
+        password TEXT,
+        email TEXT,
+        phoneNumber TEXT
+      )
+    ''');
+  }
+
+  Future<int> insertUser(Map<String, dynamic> user) async {
+    final db = await database;
+    return await db.insert('users', user);
+  }
+
+  Future<List<Map<String, dynamic>>> getUsers() async {
+    final db = await database;
+    return await db.query('users');
+  }
+
+  // In db_helper.dart
+
+Future<void> updateUserPassword(String phoneNumber, String newPassword) async {
+  final db = await database;
+  await db.update(
+    'users',
+    {'password': newPassword},
+    where: 'phoneNumber = ?',
+    whereArgs: [phoneNumber],
+  );
+}
+
+
